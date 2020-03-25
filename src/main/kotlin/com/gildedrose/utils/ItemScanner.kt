8b10317a4d630c2dfdbf841d09c065bf4687b44a
tperraut <mythom008@gmail.com>
2020-03-25 19:58:35 +0100

    [ ItemScanner ] Create tests and implement

---
+++ src/main/kotlin/com/gildedrose/utils/ItemScanner.kt
@@ -0,0 +1,19 @@
+package com.gildedrose.utils
+
+import com.gildedrose.base.BaseItem
+import com.gildedrose.model.*
+
+object ItemScanner {
+
+    private val catalog: Map<String, (item: Item) -> BaseItem> = mapOf(
+            "Sulfuras, Hand of Ragnaros" to ::LegendaryItem,
+            "Conjured Mana Cake" to ::ConjuredItem,
+            "Aged Brie" to ::AgedBrieItem,
+            "Backstage passes to a TAFKAL80ETC concert" to ::BackStageItem
+    )
+
+    fun scan(item: Item): BaseItem {
+        val mapper = catalog[item.name]
+        return mapper?.invoke(item) ?: NormalItem(item)
+    }
+}
