a1d24227f83d9d87d543a98ae5b0698bc5085fcd
tperraut <mythom008@gmail.com>
2020-03-27 21:23:45 +0100

    [ #4 ] Correct bad naming

--- src/test/kotlin/com/gildedrose/model/BackStageItemTest.kt
+++ src/test/kotlin/com/gildedrose/model/BackStageItemTest.kt
@@ -10,9 +10,9 @@ class BackStageItemTest {
     fun `should increase the item quality value by one when its sellIn value is greater than 10`() {
         // setup
         val item = Item(Fixture.tBackStageItemName, 11, 5)
-        val normalItem = BackStageItem(item)
+        val backStageItem = BackStageItem(item)
         // act
-        normalItem.update()
+        backStageItem.update()
         // assert
         assertEquals(6, item.quality)
     }
@@ -21,9 +21,9 @@ class BackStageItemTest {
     fun `should increase the item quality value by 2 when its sellIn value is equal to 10`() {
         // setup
         val item = Item(Fixture.tBackStageItemName, 10, 5)
-        val normalItem = BackStageItem(item)
+        val backStageItem = BackStageItem(item)
         // act
-        normalItem.update()
+        backStageItem.update()
         // assert
         assertEquals(7, item.quality)
     }
@@ -32,9 +32,9 @@ class BackStageItemTest {
     fun `should increase the item quality value by 2 when its sellIn value is lower than 10 and greater than 5`() {
         // setup
         val item = Item(Fixture.tBackStageItemName, 7, 5)
-        val normalItem = BackStageItem(item)
+        val backStageItem = BackStageItem(item)
         // act
-        normalItem.update()
+        backStageItem.update()
         // assert
         assertEquals(7, item.quality)
     }
@@ -43,9 +43,9 @@ class BackStageItemTest {
     fun `should increase the item quality value by 3 when its sellIn value is equal to 5`() {
         // setup
         val item = Item(Fixture.tBackStageItemName, 5, 5)
-        val normalItem = BackStageItem(item)
+        val backStageItem = BackStageItem(item)
         // act
-        normalItem.update()
+        backStageItem.update()
         // assert
         assertEquals(8, item.quality)
     }
@@ -54,9 +54,9 @@ class BackStageItemTest {
     fun `should increase the item quality value by 3 when its sellIn value is lower than 5 and greater than 0`() {
         // setup
         val item = Item(Fixture.tBackStageItemName, 2, 5)
-        val normalItem = BackStageItem(item)
+        val backStageItem = BackStageItem(item)
         // act
-        normalItem.update()
+        backStageItem.update()
         // assert
         assertEquals(8, item.quality)
     }
@@ -65,9 +65,9 @@ class BackStageItemTest {
     fun `should not increase the item quality value when its quality value is 50`() {
         // setup
         val item = Item(Fixture.tBackStageItemName, 2, 50)
-        val normalItem = BackStageItem(item)
+        val backStageItem = BackStageItem(item)
         // act
-        normalItem.update()
+        backStageItem.update()
         // assert
         assertEquals(50, item.quality)
     }
@@ -76,9 +76,9 @@ class BackStageItemTest {
     fun `should set the item quality value to 50 when its quality value exceed 50 during the update process`() {
         // setup
         val item = Item(Fixture.tBackStageItemName, 2, 49)
-        val normalItem = BackStageItem(item)
+        val backStageItem = BackStageItem(item)
         // act
-        normalItem.update()
+        backStageItem.update()
         // assert
         assertEquals(50, item.quality)
     }
@@ -87,9 +87,9 @@ class BackStageItemTest {
     fun `should drop the item quality value to 0 when its sellIn value is equal to 0`() {
         // setup
         val item = Item(Fixture.tBackStageItemName, 0, 5)
-        val normalItem = BackStageItem(item)
+        val backStageItem = BackStageItem(item)
         // act
-        normalItem.update()
+        backStageItem.update()
         // assert
         assertEquals(0, item.quality)
     }
@@ -98,9 +98,9 @@ class BackStageItemTest {
     fun `should drop the item quality value to 0 when its sellIn value is lower than 0`() {
         // setup
         val item = Item(Fixture.tBackStageItemName, -4, 5)
-        val normalItem = BackStageItem(item)
+        val backStageItem = BackStageItem(item)
         // act
-        normalItem.update()
+        backStageItem.update()
         // assert
         assertEquals(0, item.quality)
     }
@@ -109,9 +109,9 @@ class BackStageItemTest {
     fun `should decrease the item sellIn value by one when update method is called`() {
         // setup
         val item = Item(Fixture.tBackStageItemName, 4, 5)
-        val normalItem = BackStageItem(item)
+        val backStageItem = BackStageItem(item)
         // act
-        normalItem.update()
+        backStageItem.update()
         // assert
         assertEquals(3, item.sellIn)
     }
